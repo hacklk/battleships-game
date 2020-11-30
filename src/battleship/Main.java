9f9a5f7ee6c1dc83b60e4e3c84f7dfa9e93ab47c
Piotr Computer <bujan98pl@gmail.com>
2020-11-30 14:53:37 +0100

    Add check if ship is to close when inserting

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -35,8 +35,8 @@ public class Main {
         }
         printField(field);
 
-        int ship3CellSwitch = 0;
         int shipLength = 5;
+        int ship3CellSwitch = 0;
 
         while (shipLength > 1) {
 
@@ -44,10 +44,10 @@ public class Main {
             if (shipLength == 3) {
 
                 if (ship3CellSwitch == 0) {
-                    System.out.println("Enter the coordinates of the " + nameBind.get(3) + " (3 cells): ");
+                    System.out.println("Enter the coordinates of the " + nameBind.get(shipLength) + " (3 cells): ");
                     ship3CellSwitch++;
                 } else if (ship3CellSwitch == 1) {
-                    System.out.println("Enter the coordinates of the " + nameBind.get(33) + " (3 cells): ");
+                    System.out.println("Enter the coordinates of the " + nameBind.get(shipLength + 30) + " (3 cells): ");
                     ship3CellSwitch++;
                 } else if (ship3CellSwitch == 2){
                     shipLength--;
@@ -70,8 +70,15 @@ public class Main {
 
                 int getLength = 0;
 
-                if (getLetter1Int == getLetter2Int) {
-                    getLength = Math.max(getNum1, getNum2) - Math.min(getNum1, getNum2) + 1;
+                int minNum = Math.min(getNum1, getNum2);
+                int maxNum = Math.max(getNum1, getNum2);
+                int minLetterInt = Math.min(getLetter1Int, getLetter2Int);
+                int maxLetterInt = Math.max(getLetter1Int, getLetter2Int);
+
+                boolean tooClose = false;
+
+                if (getLetter1Int == getLetter2Int) {      //inserting ships horizontally
+                    getLength = maxNum - minNum + 1;
                     if (shipLength == 3 && ship3CellSwitch == 2 && getLength != shipLength) {
                         System.out.println("\nError! Wrong length of the " + nameBind.get(33) + "! Try again:");
                         continue;
@@ -80,12 +87,26 @@ public class Main {
                         continue;
                     }
 
-                    for (int i = Math.min(getNum1, getNum2); i <= Math.max(getNum1, getNum2); i++) {
+                    //check if ship is too close to another
+                    for (int i = minNum; i <= maxNum; i++) {
+                        try {
+                            if ("O".contains(field[getLetter1Int + 1][i]) || "O".contains(field[getLetter1Int - 1][i])
+                                    || "O".contains(field[getLetter1Int][i + 1]) || "O".contains(field[getLetter1Int][i - 1])) {
+                                System.out.println("\nError! You placed it too close to another one. Try again:");
+                                tooClose = true;
+                                break;
+                            }
+                        }catch (ArrayIndexOutOfBoundsException ignored) {}
+                    }
+
+                    if (tooClose) { continue; }
+
+                    for (int i = minNum; i <= maxNum; i++) {
                         field[getLetter1Int][i] = "O";
                     }
                     break;
-                }else if (getNum1 == getNum2) {
-                    getLength = Math.max(getLetter1Int, getLetter2Int) - Math.min(getLetter1Int, getLetter2Int) + 1;
+                }else if (getNum1 == getNum2) {         //inserting ships vertically
+                    getLength = maxLetterInt - minLetterInt + 1;
                     if (shipLength == 3 && ship3CellSwitch == 2 && getLength != shipLength) {
                         System.out.println("\nError! Wrong length of the " + nameBind.get(33) + "! Try again:");
                         continue;
@@ -94,7 +115,21 @@ public class Main {
                         continue;
                     }
 
-                    for (int i = Math.min(getLetter1Int, getLetter2Int); i <= Math.max(getLetter1Int, getLetter2Int); i++) {
+                    //check if ship is too close to one another vertically
+                    for (int i = minLetterInt; i <= maxLetterInt; i++) {
+                        try {
+                            if ("O".contains(field[i][getNum1 + 1]) || "O".contains(field[i][getNum1 - 1])
+                                    || "O".contains(field[i + 1][getNum1]) || "O".contains(field[i - 1][getNum1])) {
+                                System.out.println("\nError! You placed it too close to another one. Try again:");
+                                tooClose = true;
+                                break;
+                            }
+                        }catch (ArrayIndexOutOfBoundsException ignored) {}
+                    }
+
+                    if (tooClose) { continue; }
+
+                    for (int i = minLetterInt; i <= maxLetterInt; i++) {
                         field[i][getNum1] = "O";
                     }
                     break;
@@ -102,8 +137,6 @@ public class Main {
                 }else { System.out.println("\nError! Wrong ship location! Try again:"); }
 
             }
-
-            //F3 F7 - [6][3], [6][7]
 
             printField(field);
 
